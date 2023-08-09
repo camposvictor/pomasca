69424abb397355c3994c863ea9931733f334312b
Victor Hugo <vhsouza606@gmail.com>
2023-08-09 19:27:28 -0300

    feat: add header and dark mode styles to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from './components/header'
 
 function App() {
   return (
-      <main className='h-screen flex items-center justify-center font-body'>
-      </main>
+    <main className="flex h-screen justify-center bg-white px-5 py-4 font-body dark:bg-gray-900">
+      <div className="w-full max-w-lg">
+        <Header />
+      </div>
+    </main>
   )
 }
 
