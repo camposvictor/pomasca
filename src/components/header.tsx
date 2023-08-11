042a09f6c0de5249ad355348fbf62ce4ea8fbf58
Victor Hugo <vhsouza606@gmail.com>
2023-08-10 22:24:24 -0300

    feat: add settings link to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+import { Settings } from 'lucide-react'
 import { ThemeSwitcher } from './theme-switcher'
+import { Button } from './ui/button'
+import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
@@ -6,7 +9,14 @@ export function Header() {
       <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50">
         Pomasca
       </h1>
-      <ThemeSwitcher />
+      <div className="flex gap-4">
+        <Button size="icon" variant="outline" asChild disabled>
+          <Link to="/settings">
+            <Settings size={18} />
+          </Link>
+        </Button>
+        <ThemeSwitcher />
+      </div>
     </header>
   )
 }
