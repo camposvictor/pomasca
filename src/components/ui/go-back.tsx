d94e77396b898cda97e61e123a8d64bfabbe8200
Victor Hugo <vhsouza606@gmail.com>
2023-08-16 19:57:38 -0300

    fix: go back button dont working

--- src/components/ui/go-back.tsx
+++ src/components/ui/go-back.tsx
@@ -1,13 +1,14 @@
 import { ArrowLeft } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export function GoBack() {
+  const navigate = useNavigate()
   return (
-    <Link
-      to=".."
+    <button
+      onClick={() => navigate(-1)}
       className="inline-flex h-10 w-10 items-center justify-center text-gray-500 transition hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
     >
       <ArrowLeft size={20} />
-    </Link>
+    </button>
   )
 }
