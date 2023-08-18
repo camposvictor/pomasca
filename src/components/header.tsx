2daae4c8364e8ca68eed9745302eb8f1b45e89aa
Victor Hugo <vhsouza606@gmail.com>
2023-08-18 20:05:09 -0300

    refactor: rename routes to run in gh pages

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <header className="flex w-full items-baseline justify-between border-b border-gray-300 pb-2 shadow-sm dark:border-gray-700">
+    <header className="flex w-full items-baseline justify-between border-b border-gray-200 pb-2 shadow-sm dark:border-gray-800">
       <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-50">
         Pomasca
       </h1>
       <div className="flex gap-4">
         <Button size="icon" variant="outline" asChild disabled>
-          <Link to="/settings">
+          <Link to="/pomasca/settings">
             <Settings size={18} />
           </Link>
         </Button>
